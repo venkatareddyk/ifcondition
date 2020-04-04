e2b0f4f8508c912ba25f5aaf373e7ff672996770
venkatreddyk <vnkatareddy@gmail.com>
2020-04-05 00:03:57 +0530

    ifcondition

---
+++ hello.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+word=b
+
+if [ $word = "a" ]
+then
+    echo "condition a is ture"
+    
+elif [ $word = "b" ]
+then 
+    echo "condition b is true"
+
+else "condition is false"
+
+fi
+
